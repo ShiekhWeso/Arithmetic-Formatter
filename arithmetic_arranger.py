4979c36bab6d4f45bb2b56947f031e751e2717bf
Weso <mohamed.hosam.eldeeen@gmail.com>
2025-02-18 19:10:09 +0200

    Update arithmetic_arranger.py
    minor changes

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,4 @@
-def arithmetic_arranger(problems, display_answer=False):
+def arithmetic_arranger(problems, display_answers=False):
     # Error check: Too many problems
     if len(problems) > 5:
         return "Error: Too many problems"
@@ -8,7 +8,7 @@ def arithmetic_arranger(problems, display_answer=False):
     operators = []
     max_widths = []
     
-    # Process each 
+    # Process each problem
     for problem in problems:
         parts = problem.split()
         if parts [1] not in ["+", "-"]:
@@ -22,4 +22,29 @@ def arithmetic_arranger(problems, display_answer=False):
         operators.append(parts[1])
         second_operands.append(parts[2])
         max_widths.append(max(len(parts[0]), len(parts[2])) + 2)  # +2 for operator and space
-        
+    
+    # Arrange problems
+    first_line = ""
+    second_line = ""
+    dashes = ""
+    answers = ""
+    
+    for i in range(len(problems)):
+        first_line += first_operands[i].rjust(max_widths[i]) + "    "
+        second_line += operators[i] + " " + second_operands[i].rjust(max_widths[i] - 2) + "    "
+        dashes += "-" * max_widths[i] + "    "
+        if display_answers:
+            if operators[i] == "+":
+                answer = str(int(first_operands[i]) + int(second_operands[i]))
+            else:
+                answer = str(int(first_operands[i]) - int(second_operands[i]))
+            answers += answer.rjust(max_widths[i]) + "    "
+
+    arranged_problems = first_line.rstrip() + "\n" + second_line.rstrip() + "\n" + dashes.rstrip()
+    if display_answers:
+        arranged_problems += "\n" + answers.rstrip()
+
+    return arranged_problems
+
+print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True))
+print(arithmetic_arranger(["44 + 815", "909 - 2", "45 + 43", "123 + 49", "888 + 40", "653 + 87"], True))
